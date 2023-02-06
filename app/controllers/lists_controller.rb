05ca47db5136bcb685b86dce5ca2df9e206865ee
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-02-06 06:14:53 +0000

    [Finish]3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -5,11 +5,13 @@ class ListsController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-   list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-   list.save
-   redirect_to list_path(list.id)
+   @list = List.new(list_params)
+    if @list.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
   end
 
   def index
@@ -23,16 +25,22 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-   get '/top' => 'homes#top'
-    # .../lists/1 や .../lists/3 に該当する
-  get 'lists/:id' => 'lists#show',as: 'list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  
+  get '/top' => 'homes#top'
+  resources:lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
